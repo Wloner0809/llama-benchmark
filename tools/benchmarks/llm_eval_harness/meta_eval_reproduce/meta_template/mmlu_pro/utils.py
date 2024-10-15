42e1896c6e0402d329ae5c63ddea5c17fc105aa7
Wloner0809 <wloner@mail.ustc.edu.cn>
2024-10-15 06:49:11 +0000

    feat(mmlu pro): add zero shot cot

--- tools/benchmarks/llm_eval_harness/meta_eval_reproduce/meta_template/mmlu_pro/utils.py
+++ tools/benchmarks/llm_eval_harness/meta_eval_reproduce/meta_template/mmlu_pro/utils.py
@@ -27,3 +27,34 @@ def process_docs(dataset: datasets.Dataset) -> datasets.Dataset:
     dataset = dataset.rename_column("is_correct", "previously_is_correct")
     dataset = dataset.map(_process_doc)
     return dataset.map(_process_doc)
+
+
+def doc_to_text_zeroshot_cot(doc: dict) -> str:
+    prompt = (
+        "<|start_header_id|>user<|end_header_id|>\n\nGiven the following question and candidate answers, choose the best answer.\nQuestion: "
+        + doc["question"]
+    )
+    for i, option in enumerate(doc["options"]):
+        prompt += "\n{}. {}".format(chr(i + 1 + 64), option)
+    prompt += '\n\nYour response should end with "The best answer is [the_answer_letter]." where the [the_answer_letter] is a letter from the provided choices.\n\nLet\'s think step by step.<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n'
+    return prompt
+
+
+def process_docs_zeroshot_cot(dataset: datasets.Dataset) -> datasets.Dataset:
+    def _process_doc_zeroshot_cot(doc: dict) -> dict:
+        out_doc = {
+            "problem": doc["question"],
+            "gold": doc["answer"],
+        }
+        return out_doc
+
+    dataset = dataset.select_columns(
+        [
+            "question",
+            "answer",
+            "options",
+            "answer_index",
+        ]
+    )
+    dataset = dataset.map(_process_doc_zeroshot_cot)
+    return dataset.map(_process_doc_zeroshot_cot)
